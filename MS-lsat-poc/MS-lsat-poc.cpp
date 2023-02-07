644a64b47b796e47dc6f42cf2f33d289e763b357
Christopher Paschen <christopher.paschen@trustedsec.com>
2023-02-07 14:16:10 -0800

    added rpc exception handling

--- MS-lsat-poc/MS-lsat-poc.cpp
+++ MS-lsat-poc/MS-lsat-poc.cpp
@@ -31,18 +31,17 @@ RPC_UNICODE_STRING strings;
 
 void usage()
 {
-    printf("<prog.exe> <target> <svcname>");
+    printf("<prog.exe> <target>");
 }
 
 
 
-int list_names(RPC_BINDING_HANDLE* hrpc, va_list args)
+int list_names(wchar_t* target)
 {
     LSAPR_OBJECT_ATTRIBUTES lsa_obj = { 0 }; // sets root dir to null
     LSAPR_HANDLE hlsa = INVALID_HANDLE_VALUE;
-    wchar_t junk[] = L"DC01.passionlabs.org";
     HRESULT hr = S_OK;
-    RPC_UNICODE_STRING strings[3] = { 0 };
+    RPC_UNICODE_STRING strings[2] = { 0 };
     LSAPR_REVISION_INFO rev_info = { 0 }, out_rev_info = { 0 };
     rev_info.V1.Revision = 1;
     rev_info.V1.SupportedFeatures = 0;
@@ -54,18 +53,19 @@ int list_names(RPC_BINDING_HANDLE* hrpc, va_list args)
 
     make_unicode_str((PUNICODE_STRING) & (strings[0]), L"NT Service\\RasMan");
     make_unicode_str((PUNICODE_STRING) & (strings[1]), L"NT Service\\ThisSHouldNotExist");
-    make_unicode_str((PUNICODE_STRING) & (strings[2]), va_arg(args, PCWSTR));
     LSAPR_POLICY_PRIMARY_DOM_INFO pdi = { 0 };
-    hr = LsarOpenPolicy3(junk, &lsa_obj,  POLICY_LOOKUP_NAMES, 1,&rev_info, &out_rev_ver, &out_rev_info, &hlsa);
+    RpcTryExcept
+    hr = LsarOpenPolicy3(target, &lsa_obj,  POLICY_LOOKUP_NAMES, 1,&rev_info, &out_rev_ver, &out_rev_info, &hlsa);
     if (!NT_SUCCESS(hr))
     {
         printf("failed to open policy %ld\n", hr);
         return FALSE;
     }
-    hr = LsarLookupNames(hlsa, 3, strings, &ref_dom,  &ts, LsapLookupWksta, &count);
+    hr = LsarLookupNames(hlsa, 2, strings, &ref_dom,  &ts, LsapLookupWksta, &count);
     if (!NT_SUCCESS(hr))
     {
         printf("translation failed %ld\n", hr);
+        LsarClose(&hlsa);
         return FALSE;
     }
     else
@@ -74,22 +74,27 @@ int list_names(RPC_BINDING_HANDLE* hrpc, va_list args)
         for (int i = 0; i < ts.Entries; i++)
         {
             printf("%ls %s\n", strings[i].Buffer, (ts.Sids[i].DomainIndex == -1) ? "Does not exist" : "Exists");
-            //printf("%d %d %d\n", ts.Sids[i].DomainIndex, ts.Sids[i].RelativeId, ts.Sids[i].Use);
         }
     }
+    RpcExcept(RpcExceptionFilter(RpcExceptionCode()))
+        printf("An exception occured while attempting to make the RPC call, recovering and bailing: %lu\n", RpcExceptionCode());
+    RpcEndExcept
+    LsarClose(&hlsa);
 
 }
 
 int wmain(int argc, wchar_t ** argv)
 {
-    if (argc != 3)
+    if (argc != 2)
     {
         usage();
         return (1);
     }
-    //        make_rpc_request(efsrpcefsrpc_v1_0_c_ifspec, targetComputer, pipename, authtype, L"host", username, password, MS_EFSR_UUID, TRUE, EFSAuthRequest, credCatcher);
+    //If this used a more traditional interface, here is what a setup call would have looked like
+    //make_rpc_request(lsarpc_v0_0_c_ifspec, argv[1], (RPC_WSTR)(L"\\pipe\\lsarpc"), 0, L"host", NULL, NULL, L"12345778-1234-ABCD-EF00-0123456789AB", TRUE, list_names, argv[2]);
+    //but it doesn't so we just call into it
+    list_names(argv[1]);
 
-    make_rpc_request(lsarpc_v0_0_c_ifspec, argv[1], (RPC_WSTR)(L"\\pipe\\lsarpc"), 0, L"host", NULL, NULL, L"12345778-1234-ABCD-EF00-0123456789AB", TRUE, list_names, argv[2]);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
