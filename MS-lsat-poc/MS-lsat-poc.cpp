adad385fc257901e6cdebaaac3c52e3ca56ca6ca
Christopher Paschen <christopher.paschen@trustedsec.com>
2023-02-08 09:03:40 -0800

    adding some comments and cleanup

--- MS-lsat-poc/MS-lsat-poc.cpp
+++ MS-lsat-poc/MS-lsat-poc.cpp
@@ -8,7 +8,6 @@
 #include <ntstatus.h>
 #include <stdio.h>
 #include "rpc_helpers.h"
-//#include "ms-lsat.h"
 #include <sddl.h>
 #include "ms-lsar.h"
 
@@ -97,13 +96,3 @@ int wmain(int argc, wchar_t ** argv)
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- WindowsRpcHelper/rpc_helpers.h
+++ WindowsRpcHelper/rpc_helpers.h
@@ -2,6 +2,8 @@
 #include <Windows.h>
 #include <rpc.h>
 
-typedef int (*RPC_CALLBACK)(RPC_BINDING_HANDLE *hrpc, va_list args);
-void make_unicode_str(PUNICODE_STRING ustr, const wchar_t* string);
+typedef int (*RPC_CALLBACK)(RPC_BINDING_HANDLE *hrpc, va_list args); // callback function taking a va_list such that any number of args may be passed to it
+void make_unicode_str(PUNICODE_STRING ustr, const wchar_t* string); // Just a basic helper function to setup wide char strings as unicode strings
+
+//This is a helper function that will bind to a given rpc service over an smb named pipe and on success call your callback function with the connected rpc binding handle
 int make_rpc_request(RPC_IF_HANDLE spec, wchar_t* targetServer, RPC_WSTR pipename, BYTE authtype, const wchar_t* service, const wchar_t* username, const wchar_t* password, const wchar_t* uuid, BOOL use_rpc_auth, RPC_CALLBACK callback, ...);
